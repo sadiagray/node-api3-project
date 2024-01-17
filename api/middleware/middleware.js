b597933bf54bda790afeec7b6ea98471f8a7437c
Sadia Gray <sadiagray17@gmail.com>
2024-01-16 21:12:35 -0600

    completed all middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   const timestamp = new Date().toLocaleString()
   const method = req.method
@@ -6,19 +8,46 @@ function logger(req, res, next) {
   next()
 }
 
-function validateUserId(req, res, next) {
-  console.log('validateUserId middleware')
-  next()
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if(!user) {
+      res.status(404).json({
+        message: "user not found",
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Poblems inside validateUserId",
+    })
+  }
 }
 
 function validateUser(req, res, next) {
-  console.log('validateUser middleware')
-  next()
+  const { name } = req.body
+  if(!name || !name.trim()) {
+    res.status(400).json({
+      message: "missing required name field"
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  console.log('validatePost middleware')
-  next()
+  const { text } = req.body
+  if(!text || !text.trim()) {
+    res.status(400).json({
+      message: "missing required text field"
+    })
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
 
 module.exports = {
