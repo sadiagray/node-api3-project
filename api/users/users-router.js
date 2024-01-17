4141ab88e1f49a5a92d7e130a085d92fe9af7bab
Sadia Gray <sadiagray17@gmail.com>
2024-01-16 21:26:47 -0600

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -4,13 +4,18 @@ validateUserId,
 validateUser,
 validatePost,
 } = require('../middleware/middleware')
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
+
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get('/', (req, res, next) => {
+  User.get()
+  .then(users => {
+    res.json(users)
+  })
+  .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
@@ -45,5 +50,12 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // and another middleware to check that the request body is valid
 });
 
-// do not forget to export the router
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    customMessage: 'Oops, something went wrong inside router',
+    message: err.message,
+    stack: err.stack,
+  })
+})
+
 module.exports = router;
