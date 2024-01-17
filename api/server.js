52e4cebbbc8bd673efd9fbc91eeeddb0e3854b22
Sadia Gray <sadiagray@Sadias-MBP.home>
2024-01-16 20:20:49 -0600

    added logger

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const { logger } = require('./middleware/middleware');
 const server = express();
 
 server.use(express.json())
-// global middlewares and the user's router need to be connected here
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
